dc210d5a4544dd39c09b13c87c2e91c666b6e431
Jainam Jain <jainjainam412@gmail.com>
2026-09-01 19:33:15 +0530

    Types: Add comments for application view routing

--- src/types.ts
+++ src/types.ts
@@ -70,3 +70,4 @@ export const PERSONAS: Persona[] = [
 // Type definition marker: Schemes and Units
 // Type definition marker: Personas and Memberships
 // Type definition marker: Role Definitions
+// Type definition marker: Navigation Views
